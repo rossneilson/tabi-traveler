38c2024863c5d32ff0d8cea8aa4131ac06dd2cc1
Ross Neilson <rossneilson99@gmail.com>
2020-07-07 22:39:41 +0100

    fix create page

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,14 +14,26 @@ exports.onCreatePage = ({ page, actions }) => {
   })
 }
 
-// blog posts
 exports.createPages = ({ graphql, actions }) => {
+  console.log("CREATING PRINTS")
   const { createPage } = actions
+  const PrintPageTemplate = path.resolve(`src/templates/PrintPage.js`)
   const BlogPostTemplate = path.resolve(`src/templates/BlogPost.js`)
   return graphql(
     `
       query MyQuery {
-        allMarkdownRemark(
+        prints: allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/(/store/)/" } }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
+            }
+          }
+        }
+        blog: allMarkdownRemark(
           filter: { fileAbsolutePath: { regex: "/(/blog/)/" } }
         ) {
           edges {
@@ -39,51 +51,26 @@ exports.createPages = ({ graphql, actions }) => {
       throw result.errors
     }
 
-    result.data.allMarkdownRemark.edges.forEach(edge => {
-      const path = edge.node.frontmatter.path
-
-      createPage({
-        path: path,
-        component: BlogPostTemplate,
-        context: {
-          slug: path,
-        },
-      })
-    })
-  })
-}
-
-// prints
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const PrintPageTemplate = path.resolve(`src/templates/PrintPage.js`)
-  return graphql(
-    `
-      query MyQuery {
-        allMarkdownRemark(
-          filter: { fileAbsolutePath: { regex: "/(/store/)/" } }
-        ) {
-          edges {
-            node {
-              frontmatter {
-                path
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-
-    result.data.allMarkdownRemark.edges.forEach(edge => {
+    // Prints
+    result.data.prints.edges.forEach(edge => {
       const path = edge.node.frontmatter.path
 
       createPage({
         path: path,
         component: PrintPageTemplate,
+        context: {
+          slug: path,
+        },
+      })
+    })
+
+    // Blog posts
+    result.data.blog.edges.forEach(edge => {
+      const path = edge.node.frontmatter.path
+
+      createPage({
+        path: path,
+        component: BlogPostTemplate,
         context: {
           slug: path,
         },
