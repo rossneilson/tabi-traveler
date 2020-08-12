9bef0395c8f26534309116f243c43d029ac9059e
Ross Neilson <rossneilson99@gmail.com>
2020-08-12 12:51:14 +0100

    fixing deepscan issues

--- src/components/common/Toggle.js
+++ src/components/common/Toggle.js
@@ -57,15 +57,15 @@ export default function Toggle({
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-language"
+        className="icon icon-tabler icon-tabler-language"
         width="48"
         height="48"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke={colour}
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" />
         <path d="M5 7h7m-2 -2v2a5 8 0 0 1 -5 8m1 -4a7 4 0 0 0 6.7 4" />
